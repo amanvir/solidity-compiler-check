d8431bf16b32ad262fc650c2e48ba74c2bb99140
Amanvir <hello@amanvir.io>
2019-04-01 10:53:38 +0100

    remove test files

--- blah.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.23;
-
-contract test {
-    uint256 a;
-    function f() {}
-}
--- test2.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.20;
-
-contract test {
-    uint256 a;
-    function f() {}
-}
